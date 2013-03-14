1eaf0caf412ec721d21204998399ec1b6da1b507
Leonardo <leonardo311@gmail.com>
2013-03-13 21:50:31 -0300

    FIX: Corrección envío de mail a administradores

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,6 @@
 # Funciones de envío de mail
 class UserMailer < ActionMailer::Base
-	default from: "contacto@reframe.cl",
-			:to => User.where('perfil = 1').map(&:email)
+	default from: "contacto@reframe.cl"
 
 	# Mail de bienvenida al usuario
 	def welcome_email(user)
@@ -13,13 +12,13 @@ class UserMailer < ActionMailer::Base
   	# Mail para solicitar bot
   	def solicitar_bot(user)
   		@user = user
-  		mail(:subject => "Solicitud Nuevo Bot de: #{@user.name}")
+  		mail(:to => User.where('perfil = 1').map(&:email), :subject => "Solicitud Nuevo Bot de: #{@user.name}")
   	end
 
   	# Mail upgrade plus
   	def upgrade_bot(user, bot)
   		@user = user
   		@bot = bot
-  		mail(:subject => "Upgrade a Plus de #{@user.name}")
+  		mail(:to => User.where('perfil = 1').map(&:email), :subject => "Upgrade a Plus de #{@user.name}")
   	end
 end
